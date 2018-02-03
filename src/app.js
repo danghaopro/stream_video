f66181d80da5ea8dfd9c5d6d2cdb71742e6a3a65
Nguyễn Đăng Hào <danghaopro97@gmail.com>
2018-02-03 10:25:18 +0700

    simple peer connect

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 const openStream = require('./openStream');
 
-openStream();
+const $ = require('jquery');
+// openStream();
 
 const Peer = require('simple-peer');
 
@@ -8,7 +9,20 @@ const p = new Peer({
   initiator: location.hash === '#1',
   trickle: false
 });
-
 p.on('signal', (token) => {
-  console.log(token);
+  var strToken = JSON.stringify(token);
+  $('#txtMySignal').val(strToken);
 });
+p.on('connect', () => {
+  console.log('connected');
+  setInterval(() => {
+    p.send(Math.random());
+  }, 2000);
+});
+p.on('data', (data) => {
+  console.log('NHAN DU LIEU: ' + data);
+});
+$('#btnConnect').click(() => {
+  const friendSign = JSON.parse($('#txtFriendSignal').val());
+  p.signal(friendSign);
+})
